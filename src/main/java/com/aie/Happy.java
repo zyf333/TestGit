f82c32331f0147b6120787d73b4816e38a1ca3ea
tom_glb <goodMorning_glb@atguigu.com>
2020-06-06 21:55:04 +0800

    1

--- src/main/java/com/aie/Happy.java
+++ src/main/java/com/aie/Happy.java
@@ -3,8 +3,6 @@ package com.aie;
 public class Happy {
 
 	public static void main(String[] args) {
-		System.out.println("right ...");
-		System.out.println("left ...");
-		System.out.println("hot fix ..............................");
+		System.out.println("master ...");
 	}
 }
