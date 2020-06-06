f6b4c1de8c55bca28e4c3c71f10b212f4ffd9039
eclipse_user@atguigu.com <eclipse_user>
2020-06-06 21:15:05 +0800

    111

--- src/main/java/com/aie/Happy.java
+++ src/main/java/com/aie/Happy.java
@@ -4,6 +4,6 @@ public class Happy {
 
 	public static void main(String[] args) {
 		System.out.println("left ...");
-		System.out.println("hot fix ...");
+		System.out.println("hot fix ..............................");
 	}
 }
